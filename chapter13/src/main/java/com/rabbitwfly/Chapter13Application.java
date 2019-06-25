8adc3a9eaab54faf3e118452313b889cceb8a82d
chentao <chentao@daokoudai.com>
2019-06-25 11:15:52 +0800

    强大的 actuator 服务监控与管理

---
+++ chapter13/src/main/java/com/rabbitwfly/Chapter13Application.java
@@ -0,0 +1,28 @@
+package com.rabbitwfly;
+
+import com.rabbitwfly.endpoint.MyEndPoint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class Chapter13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter13Application.class, args);
+    }
+
+    @Configuration
+    static class MyEndpointConfiguration{
+        @Bean
+        @ConditionalOnMissingBean
+        @ConditionalOnEnabledEndpoint
+        public MyEndPoint myEndPoint(){
+            return new MyEndPoint();
+        }
+    }
+
+}
