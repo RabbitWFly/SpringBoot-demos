ae495c3192c6dbeefb8634f7cd0bfe376e9c916b
chentao <chentao@daokoudai.com>
2019-06-25 10:06:06 +0800

    RabbitMQ延迟队列

---
+++ chapter12/src/main/java/com/rabbitwfly/entity/Book.java
@@ -0,0 +1,29 @@
+package com.rabbitwfly.entity;
+
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = -2164058270260403156L;
+
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
