590e99363fd6dd25310bf87ba87c01621b6df3d5
chentao <chentao@daokoudai.com>
2019-06-27 11:17:28 +0800

    轻松搞定重复提交（本地锁)

---
+++ chapter21/src/main/java/com/rabbitwfly/annotation/LocalLock.java
@@ -0,0 +1,23 @@
+package com.rabbitwfly.annotation;
+
+
+import java.lang.annotation.*;
+
+/**
+ * 锁的注解
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface LocalLock {
+
+    String key() default "";
+
+    /**
+     * 过期时间 TODO 由于用的 guava 暂时就忽略这属性吧 集成 redis 需要用到
+     * @return
+     */
+    int expire() default 5;
+}
