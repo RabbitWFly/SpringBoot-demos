994a415d1ec6899e240e18d6dc8010940fc92fd6
chentao <chentao@daokoudai.com>
2019-06-27 19:07:42 +0800

    JDK8 日期格式化

---
+++ chapter28/src/main/java/com/rabbitwfly/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.rabbitwfly.controller;
+
+import com.rabbitwfly.pojo.Order;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping
+    public Order query(){
+        Order order = new Order();
+        order.setPayTime(LocalDateTime.now());
+        return order;
+    }
+}
+
---
+++ chapter28/src/main/java/com/rabbitwfly/pojo/Order.java
@@ -0,0 +1,21 @@
+package com.rabbitwfly.pojo;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class Order {
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime payTime;
+
+    public LocalDateTime getPayTime() {
+        return payTime;
+    }
+
+    public void setPayTime(LocalDateTime payTime) {
+        this.payTime = payTime;
+    }
+}
+
